a10361e748aecc78ff0aae686adc4500825a0628
Uday-sidagana <uday.sidgana@gmail.com>
2025-04-13 04:12:30 +0530

    Values are gettiung inserted

--- sqlite.py
+++ sqlite.py
@@ -5,11 +5,13 @@ conn = sqlite3.connect('employee.db')
 
 c= conn.cursor()
 
-c.execute(""" CREATE TABLE employees (
-          first text,
-          last text,
-          pay integer)     
-           """)
+# c.execute(""" CREATE TABLE employees (
+#           first text,
+#           last text,
+#           pay integer)     
+#            """)
+
+c.execute(" INSERT INTO employees VALUES ('uday', 'sidagana', 5000) ")
 
 conn.commit()
 conn.close()
