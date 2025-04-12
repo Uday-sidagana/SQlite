b0f9965f6ad2a121f51575265bd9d4696067c731
Uday-sidagana <uday.sidgana@gmail.com>
2025-04-13 05:28:37 +0530

    insert and fetch functions created

--- sqlite.py
+++ sqlite.py
@@ -6,6 +6,22 @@ conn = sqlite3.connect(':memory:')
 
 c= conn.cursor()
 
+def insert_emp(emp):
+    with conn:
+        c.execute(" INSERT INTO employees VALUES (:first, :last, :pay)", {'first':emp.first, 'last': emp.last, 'pay': emp.pay})
+
+def fetch_emp(param, value):
+    with conn:
+        c.execute(" SELECT * FROM employees WHERE param=:param", {'param': 'value'})
+        return c.fetchall()
+
+
+c.execute(""" CREATE TABLE employees (
+          first text,
+          last text,
+          pay integer)     
+           """)
+
 #from employee module
 emp1 = Employee('sumit', 'shukla', 1500)
 emp2 = Employee('aditya', 'dikhit', 10)
@@ -15,11 +31,7 @@ c.execute(" INSERT INTO employees VALUES ('{}','{}',{})".format(emp1.first, emp1
 c.execute(" INSERT INTO employees VALUES (?,?,?)", (emp2.first, emp2.last, emp2.pay))
 c.execute(" INSERT INTO employees VALUES (:first, :last, :pay)", {'first':emp3.first, 'last': emp3.last, 'pay': emp3.pay})
 
-c.execute(""" CREATE TABLE employees (
-          first text,
-          last text,
-          pay integer)     
-           """)
+
 
 c.execute(" INSERT INTO employees VALUES ('uday', 'sidagana', 5000) ")
 c.execute(" INSERT INTO employees VALUES ('sam', 'lmao', 3000) ")
@@ -30,6 +42,9 @@ print(c.fetchall())
 
 c.execute(" SELECT * FROM employees WHERE last=:last", {'last': 'man'})
 print(c.fetchall())
+
+c.execute(" SELECT * FROM employees ")
+print(c.fetchall)
 # c.fetchall()
 # c.fetchone()
 # c.fetchmany(5)
