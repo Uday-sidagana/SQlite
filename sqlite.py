0bcefc23cfbe32dd7325fca9c1010ec5a5de846b
Uday-sidagana <uday.sidgana@gmail.com>
2025-04-13 05:02:05 +0530

    Creating a db in memory

--- sqlite.py
+++ sqlite.py
@@ -1,8 +1,8 @@
 import sqlite3
 from employee import Employee
 
-# conn = sqlite3.connect(':memory:')
-conn = sqlite3.connect('employee.db')
+conn = sqlite3.connect(':memory:')
+# conn = sqlite3.connect('employee.db')
 
 c= conn.cursor()
 
@@ -11,19 +11,19 @@ emp1 = Employee('sumit', 'shukla', 1500)
 emp2 = Employee('aditya', 'dikhit', 10)
 emp3 = Employee('ketchup', 'man', 9999)
 
-# c.execute(" INSERT INTO employees VALUES ('{}','{}',{})".format(emp1.first, emp1.last, emp1.pay)) 
-# c.execute(" INSERT INTO employees VALUES (?,?,?)", (emp2.first, emp2.last, emp2.pay))
-# c.execute(" INSERT INTO employees VALUES (:first, :last, :pay)", {'first':emp3.first, 'last': emp3.last, 'pay': emp3.pay})
+c.execute(" INSERT INTO employees VALUES ('{}','{}',{})".format(emp1.first, emp1.last, emp1.pay)) 
+c.execute(" INSERT INTO employees VALUES (?,?,?)", (emp2.first, emp2.last, emp2.pay))
+c.execute(" INSERT INTO employees VALUES (:first, :last, :pay)", {'first':emp3.first, 'last': emp3.last, 'pay': emp3.pay})
 
-# c.execute(""" CREATE TABLE employees (
-#           first text,
-#           last text,
-#           pay integer)     
-#            """)
+c.execute(""" CREATE TABLE employees (
+          first text,
+          last text,
+          pay integer)     
+           """)
 
-# c.execute(" INSERT INTO employees VALUES ('uday', 'sidagana', 5000) ")
-# c.execute(" INSERT INTO employees VALUES ('sam', 'lmao', 3000) ")
-# c.execute(" INSERT INTO employees VALUES ('jakarta', 'sidagana', 5000) ")
+c.execute(" INSERT INTO employees VALUES ('uday', 'sidagana', 5000) ")
+c.execute(" INSERT INTO employees VALUES ('sam', 'lmao', 3000) ")
+c.execute(" INSERT INTO employees VALUES ('jakarta', 'sidagana', 5000) ")
 
 c.execute(" SELECT * FROM employees WHERE last=?", ('sidagana',))
 print(c.fetchall())
